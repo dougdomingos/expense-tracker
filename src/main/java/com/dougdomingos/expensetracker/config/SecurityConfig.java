7123338a4c18bd2a4e311e5fde5f949a6f53c9b6
Douglas Domingos <doug.domingossilva@gmail.com>
2024-09-29 14:44:49 -0300

    config: add header frame options configuration

--- src/main/java/com/dougdomingos/expensetracker/config/SecurityConfig.java
+++ src/main/java/com/dougdomingos/expensetracker/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -37,10 +38,13 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
-                .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
                 .csrf(csrf -> csrf.disable())
+                .headers((headers) -> headers.frameOptions((frame) -> frame.sameOrigin()))
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(PathRequest.toH2Console()).permitAll()
+                        .anyRequest().authenticated())
                 .build();
     }
 
