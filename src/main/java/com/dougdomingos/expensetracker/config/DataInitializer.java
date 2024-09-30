a348144ae4b9ff7ad6fbd46b77cb44e8c3e09ee0
Douglas Domingos <doug.domingossilva@gmail.com>
2024-09-30 10:19:01 -0300

    feat: add method for creating default admin user

--- src/main/java/com/dougdomingos/expensetracker/config/DataInitializer.java
+++ src/main/java/com/dougdomingos/expensetracker/config/DataInitializer.java
@@ -1,42 +1,80 @@
 package com.dougdomingos.expensetracker.config;
 
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
+import java.util.Optional;
+import java.util.Set;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import com.dougdomingos.expensetracker.entities.user.Role;
 import com.dougdomingos.expensetracker.entities.user.Role.TypeRole;
+import com.dougdomingos.expensetracker.entities.user.User;
 import com.dougdomingos.expensetracker.repositories.RolesRepository;
+import com.dougdomingos.expensetracker.repositories.UserRepository;
 
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 
 @Component
 @RequiredArgsConstructor
-public class DataInitializer implements ApplicationRunner {
+public class DataInitializer implements CommandLineRunner {
 
     private final RolesRepository roles;
 
+    private final UserRepository users;
+
+    private final BCryptPasswordEncoder passwordEncoder;
+
     @Override
-    public void run(ApplicationArguments args) throws Exception {
+    @Transactional
+    public void run(String... args) throws Exception {
         initRolesInDB();
+        createAdminUser();
     }
 
     /**
      * Insert the user roles into the database if not present.
      */
     private void initRolesInDB() {
-        if (!roles.findByRoleName(TypeRole.ROLE_USER).isPresent()) {
+        if (!roles.findByRoleName(TypeRole.USER).isPresent()) {
             Role userRole = Role.builder()
-                    .roleName(Role.TypeRole.ROLE_USER)
+                    .roleName(Role.TypeRole.USER)
                     .build();
             roles.save(userRole);
         }
 
-        if (!roles.findByRoleName(TypeRole.ROLE_ADMIN).isPresent()) {
+        if (!roles.findByRoleName(TypeRole.ADMIN).isPresent()) {
             Role adminRole = Role.builder()
-                    .roleName(Role.TypeRole.ROLE_ADMIN)
+                    .roleName(Role.TypeRole.ADMIN)
                     .build();
             roles.save(adminRole);
         }
+    }
+
+    /**
+     * Create the Admin user.
+     */
+    private void createAdminUser() {
+        Optional<Role> adminRole = roles.findByRoleName(Role.TypeRole.ADMIN);
+        Optional<User> userAdmin = users.findByUsername("admin");
+
+        if (!adminRole.isPresent()) {
+            throw new RuntimeException("Admin role not created");
+        }
+
+        userAdmin.ifPresentOrElse(
+                (user) -> {
+                    System.out.println("Admin is already created!");
+                },
+                () -> {
+                    User admin = User.builder()
+                            .username("admin")
+                            .password(passwordEncoder.encode("admin"))
+                            .roles(Set.of(adminRole.get()))
+                            .build();
+
+                    users.save(admin);
+                });
     }
 }
