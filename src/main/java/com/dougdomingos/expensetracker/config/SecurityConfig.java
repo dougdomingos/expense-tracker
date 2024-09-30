364b6b1ae8986ca0e3d8a0fd82ba8fd397c14753
Douglas Domingos <doug.domingossilva@gmail.com>
2024-09-30 10:18:32 -0300

    refactor: group routes inside request matcher

--- src/main/java/com/dougdomingos/expensetracker/config/SecurityConfig.java
+++ src/main/java/com/dougdomingos/expensetracker/config/SecurityConfig.java
@@ -38,15 +38,20 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(
+                                "/users",
+                                "/users/login",
+                                "/v3/api-docs/**",
+                                "/swagger-ui.html",
+                                "/swagger-ui/**")
+                        .permitAll()
+                        .requestMatchers(PathRequest.toH2Console()).permitAll()
+                        .anyRequest().authenticated())
                 .csrf(csrf -> csrf.disable())
                 .headers((headers) -> headers.frameOptions((frame) -> frame.sameOrigin()))
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/**").permitAll()
-                        .requestMatchers("/users").permitAll()
-                        .requestMatchers(PathRequest.toH2Console()).permitAll()
-                        .anyRequest().authenticated())
                 .build();
     }
 
