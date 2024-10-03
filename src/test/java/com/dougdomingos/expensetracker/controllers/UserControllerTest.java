3ded69780f3a2cd4744156b404fa896a3f4de071
Douglas Domingos <doug.domingossilva@gmail.com>
2024-10-03 13:37:46 -0300

    refactor: remove unused ModelMapper attribute

--- src/test/java/com/dougdomingos/expensetracker/controllers/UserControllerTest.java
+++ src/test/java/com/dougdomingos/expensetracker/controllers/UserControllerTest.java
@@ -13,7 +13,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -45,9 +44,6 @@ public class UserControllerTest {
 
     @Autowired
     MockMvc driver;
-
-    @Autowired
-    ModelMapper mapper;
 
     @Autowired
     UserRepository userRepository;
