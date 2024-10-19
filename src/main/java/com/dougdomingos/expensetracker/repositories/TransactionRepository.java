5aac3a31d6407b9ee07e7a3076853a79c7237d54
Douglas Domingos <doug.domingossilva@gmail.com>
2024-10-19 12:01:34 -0300

    feat: add implementation of balance feature

--- src/main/java/com/dougdomingos/expensetracker/repositories/TransactionRepository.java
+++ src/main/java/com/dougdomingos/expensetracker/repositories/TransactionRepository.java
@@ -1,5 +1,6 @@
 package com.dougdomingos.expensetracker.repositories;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,9 +9,31 @@ import com.dougdomingos.expensetracker.entities.transaction.Transaction;
 import com.dougdomingos.expensetracker.entities.transaction.TransactionType;
 import com.dougdomingos.expensetracker.entities.user.User;
 
-
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+
+    /**
+     * Fetch all transactions of a specific user.
+     * 
+     * @param owner The user that owns the transactions
+     * @return A list of transactions
+     */
     List<Transaction> findByOwner(User owner);
 
+    /**
+     * Fetch transactions of a specific user, filtered by type.
+     * 
+     * @param owner The user that owns the transactions
+     * @param type  The targeted transaction type
+     * @return A list of transactions that match the given type
+     */
     List<Transaction> findByOwnerAndTransactionType(User owner, TransactionType type);
+
+    /**
+     * Fetch all transactions created in a specified time interval.
+     * 
+     * @param start The base date of the interval
+     * @param end   The limit date of the interval
+     * @return A list of transactions that match the given interval
+     */
+    List<Transaction> findByOwnerAndCreatedAtBetween(User owner, LocalDateTime start, LocalDateTime end);
 }
